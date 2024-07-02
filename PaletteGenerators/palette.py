58d6beb6b00807f7a458ad25785eb285b14b5fea
paulderry <63982665+paulderry@users.noreply.github.com>
2024-07-02 08:01:30 -0500

    Update palette.py

--- PaletteGenerators/palette.py
+++ PaletteGenerators/palette.py
@@ -18,8 +18,6 @@ def write_gimp_palette(triplets):
 
 def write_paintdotnet_palette(triplets):
     with codecs.open(sys.argv[2], mode='w', encoding="utf-8") as file:
-        file.write("GIMP Palette\n")
-        file.write(f"Name: {sys.argv[1]}\n")
         for line in triplets:
             file.write(f"FF{line[0] << 2}{line[1] << 2}{line[2] << 2}\n")
 
